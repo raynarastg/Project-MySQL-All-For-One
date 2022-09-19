3e70475067836e58c01f9f611c3166b463ca675e
Raynara Santiago <raysantiagobz@gmail.com>
2022-09-19 16:11:59 -0300

    desafio 11 outra tentativa

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,1 @@
-SELECT notes FROM purchase_orders WHERE notes = 'Purchase generated based on Order' BETWEEN 30 AND 39;
+SELECT notes FROM purchase_orders WHERE notes = 'Purchase generated based on Order' >= 30 AND <= 39;
